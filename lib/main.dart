2c77b977649004f70671d025e6e505ee7fdd9937
Joel Sebastian Villarojo <sebastianvillarojo16@gmail.com>
2024-06-07 12:19:22 +0800

    Finalized barangay Services Location

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,29 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:purokonek/Constants/API_KEY.dart';
+import 'package:purokonek/Functions/initGoogleMaps.dart';
 import 'package:purokonek/Screens/chat.dart';
 import 'package:purokonek/Screens/homepage.dart';
 import 'package:purokonek/Screens/maps_page.dart';
 import 'package:purokonek/Screens/onboarding.dart';
+import 'package:google_maps_flutter_android/google_maps_flutter_android.dart';
+import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
 
-void main() {
+void main() async {
   Gemini.init(apiKey: GEMINI_API_KEY);
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isAndroid) {
+    final GoogleMapsFlutterPlatform mapsImplementation =
+        GoogleMapsFlutterPlatform.instance;
+    if (mapsImplementation is GoogleMapsFlutterAndroid) {
+      mapsImplementation.useAndroidViewSurface = true;
+      initializeMapRenderer();
+    }
+  }
+
   runApp(const MyApp());
 }
 
@@ -16,7 +32,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: const ChatBot(), routes: {
+    return MaterialApp(home: const MapsPage(), routes: {
       '/onboarding': (context) => const Onboarding(),
       '/homepage': (context) => const Homepage(),
       '/chat': (context) => const ChatBot(),
