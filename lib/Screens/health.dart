8307af6d260a206d057896f1d16e836246efa171
Cleverson Sucalit <cleversonsucalit@gmail.com>
2024-06-07 13:25:53 +0800

    health and homepage

--- lib/Screens/health.dart
+++ lib/Screens/health.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HealthPage extends StatefulWidget {
   const HealthPage({super.key});
@@ -10,6 +13,75 @@ class HealthPage extends StatefulWidget {
 class _HealthPageState extends State<HealthPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final Size screenSize = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            color: const Color(0xFFFFF3EB),
+            width: screenSize.width,
+            height: screenSize.height,
+          ),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Container(
+                      width: 100,
+                      child: Row(
+                        children: [
+                          Image.asset(
+                            'assets/icons/health_icon.png',
+                            width: 25,
+                            height: 25,
+                          ),
+                          const Spacer(),
+                          Text(
+                            'Health',
+                            style: GoogleFonts.inter(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: const Color(0xFFEC4A48)),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Material(
+                  elevation: 5.0, // Adjust elevation as needed
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(30.0),
+                    topRight: Radius.circular(30.0),
+                  ),
+                  child: Container(
+                    width: double
+                        .infinity, // Use double.infinity to match screen width
+                    height: 700,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30.0),
+                        topRight: Radius.circular(30.0),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(30),
+                      child: Column(
+                        children: [Text('data')],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
