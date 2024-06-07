b8a0626440aeb91c56b7804a2f6dc31d9d2f535f
Joel Sebastian Villarojo <sebastianvillarojo16@gmail.com>
2024-06-07 08:48:31 +0800

    pushh!!

---
+++ lib/Constants/API_KEY.dart
@@ -0,0 +1,2 @@
+String GEMINI_API_KEY = 'AIzaSyBeG7l1plYZ16XcSYxEAyaNSeJN7P24koM';
+String MAP_API_KEY = 'AIzaSyB7yQz_7PZnwN1PRyDuvfq0oMuZ_qryVSE';
---
+++ lib/Constants/styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const Color kWhite = Color(0xFFFFFFFF);
+const Color kDarkGrey = Color(0xFF282828);
+const Color kLightGrey = Color(0xFFE5E5E5);
+
+const Color kBlack = Color(0xFF121212);
+const Color kGrey = Color(0xff777777);
+const Color kLightYelloy = Color(0xFFFAFF00);
+const Color kGreen = Color(0xFFD4EB27);
+
+final SenMedium = GoogleFonts.sen(
+  color: kWhite,
+  fontWeight: FontWeight.w500,
+);
+
+final SenSemiBold = GoogleFonts.sen(
+  color: kWhite,
+  fontWeight: FontWeight.w600,
+);
